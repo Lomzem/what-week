92db440e9ba6bb19fd1debffa111beb3ca575cf0
Lawjay Lee <lawjaylee1@icloud.com>
2026-03-10 16:14:17 -0700

    feat: current date

--- src/script.js
+++ src/script.js
@@ -3,5 +3,9 @@ import { getWeekNumber, FIRST_DAY } from "./utils.js";
 const CURRENT_DAY = new Date();
 
 /** @type {HTMLSpanElement | null} */
-const weekNum = document.querySelector("#weekNum");
-weekNum.textContent = getWeekNumber(FIRST_DAY, CURRENT_DAY);
+const currentDateElem = document.querySelector("#currentDate");
+currentDateElem.textContent = CURRENT_DAY.toDateString();
+
+/** @type {HTMLSpanElement | null} */
+const weekNumElem = document.querySelector("#weekNum");
+weekNumElem.textContent = getWeekNumber(FIRST_DAY, CURRENT_DAY);
