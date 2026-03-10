b0dd70fa11736ed8d0aed318de79e452a6fdaca4
Lawjay Lee <lawjaylee1@icloud.com>
2026-03-10 16:23:29 -0700

    deploy

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import { viteSingleFile } from 'vite-plugin-singlefile';
 
 export default defineConfig({
+  base: '/what-week/',
   plugins: [viteSingleFile()],
   build: {
     minify: 'esbuild'
