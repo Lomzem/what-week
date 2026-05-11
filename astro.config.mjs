63c3ed297dbd19f7576fc064e2d3e46f7d1db80d
Lawjay Lee <lawjaylee1@icloud.com>
2026-05-11 04:26:57 -0700

    run formatter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,12 @@
 // @ts-check
 import { defineConfig } from "astro/config"
 import tailwindcss from "@tailwindcss/vite"
-import singleFile from "astro-single-file";
+import singleFile from "astro-single-file"
 
 // https://astro.build/config
 export default defineConfig({
-    vite: {
-        plugins: [tailwindcss()],
-    },
-    integrations: [singleFile()],
+  vite: {
+    plugins: [tailwindcss()],
+  },
+  integrations: [singleFile()],
 })
